fc568b7d2330b09353acc0329c5973851bcf0d68
BT_1N3_24 <sample@bths.edu>
2025-01-10 10:35:33 -0500

    added lines

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hi");
         System.out.println("greetings");
+        int x = 1+3;
+        System.out.println("x = " + x);
     }
 }
